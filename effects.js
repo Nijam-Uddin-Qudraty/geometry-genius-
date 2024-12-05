9e75b373875a698edd59f46bdcf697a1f9874e0a
Nijam-Uddin-Qudraty <129588187+Nijam-Uddin-Qudraty@users.noreply.github.com>
2024-12-05 13:58:07 +0600

    mathmetical function finished

--- effects.js
+++ effects.js
@@ -1,8 +1,33 @@
 
 
-// const changeColor = (e) => {
-//     // targetElement = getTargetId(e);
-//     e.classList.add("bg-blue-200");
+const changeColor = (e) => {
+    e.classList.add("bg-info");
+}
+console.log("connected");
+
+const showAns = (parent, ans) => {
+    const listId = document.getElementById("list");
+
+    // Construct the ID of the list item to update (e.g., 'triangle1' for the triangle shape)
+    const listItemId = `${parent.id}1`; 
+
+    // Find the list item by its ID
+    const listItem = document.getElementById(listItemId);
     
-// }
-// console.log("connected");
+    if (listItem && listItem.innerText === "") {  // Only update if the list item is empty
+        listItem.innerText = `Area of ${parent.id} is ${ans}`;  // Update the list item with the result
+        console.log(`Updated item: ${listItem.innerText}`);
+    } else if (listItem) {
+        console.log("Item already has text, not updating.");
+    } else {
+        console.log("List item not found.");
+    }
+};
+
+        // listItem.innerText = `Area of ${parent.id} is ${ans}`;
+        // // console.log(listItem.innerText);
+        // console.log(listItem);
+        // listId.appendChild(listItem);
+    
+    // console.log(typeof(listItem.id));
+    
--- script.js
+++ script.js
@@ -4,15 +4,20 @@ const getTargetId = (e) => {
     return targetElement
 }
 const FirstValue = (parent) => {
-    const input1 = parent.children[1].children[2].children[0];
-    const value1 = parseInt(input1.value);
-    return value1
+    // const input = parent.children[1].children[2].children[0];
+    // const value = parseInt(input.value);
+    // return value
+    const inputs = parent.querySelectorAll("input"); // Find all <input> elements inside the "triangle" div
+    return parseFloat(inputs[0].value);
+
 }
 const SecondValue = (parent) => {
     
-    const input2 = parent.children[1].children[2].children[2];
-    const value2 = parseInt(input2.value);
-    return value2
+    // const input = parent.children[1].children[2].children[2];
+    // const value = parseInt(input.value);
+    // return value
+    const inputs = parent.querySelectorAll("input"); // Find all <input> elements inside the "triangle" div
+    return parseFloat(inputs[1].value);
 }
 
 // const card = (e) => {
@@ -20,14 +25,44 @@ const SecondValue = (parent) => {
 //     changeColor(targeTid);
 
 // }
+const textFound = id => {
+    if (!id.innerText == "")
+    {
+        id.classList.remove("hidden");
+    }
+}
+
 const button = (e) => {
     const target = e.currentTarget;
     const parent = (target.parentNode);
     // console.log(parent.id);
+    const warningMsg = document.getElementById("warning").querySelector("span");
+    
+    
     value1 = FirstValue(parent);
     value2 = SecondValue(parent);
-    const ans = Calculator(parent,value1,value2)
-    console.log(ans);
+    if ( isNaN(value1) || isNaN(value2) ) {
+        console.log("please provide two valid input");
+        warningMsg.innerText = "Please provide two valid input";
+        textFound(warningMsg);
+
+    }
+    else if (value1 <= 0 || value2 <= 0)
+    {
+        console.log("Please provide a positive number");
+        warningMsg.innerText = "Please provide a positive number";
+        textFound(warningMsg);
+    }
+    else {
+        changeColor(parent);
+        const ans = Calculator(parent, value1, value2);
+        warningMsg.innerText=""
+        showAns(parent,ans)
+    }
+    
+    
+
+
     // target.parent
 }
 // console.log("connected");
